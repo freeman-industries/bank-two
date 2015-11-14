ce1836dbaeb9fe4083d48cb3765a4ddaecdab1b2
Nabil Freeman <nabil@instafork.com>
2015-11-14 15:22:44 +0000

    google analytics

--- public/assets/js/core.js
+++ public/assets/js/core.js
@@ -2,6 +2,21 @@ NodeList.prototype.forEach = Array.prototype.forEach;
 
 var _bank = {};
 
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-70129105-1', 'auto');
+ga('send', 'pageview');
+
 _bank.formatDate = function(date_string){
   return date_string.replace("Mon", "Monday,").replace("Tue", "Tuesday,").replace("Wed", "Wednesday,").replace("Thu", "Thursday,").replace("Fri", "Friday,").replace("Sat", "Saturday,").replace("Sun", "Sunday,").replace("Jan", "January").replace("Feb", "February").replace("Mar", "March").replace("Apr", "April").replace("Jun", "June").replace("Jul", "July").replace("Aug", "August").replace("Sep", "September").replace("Oct", "October").replace("Nov", "November").replace("Dec", "December");
 };
@@ -143,6 +158,10 @@ document.querySelector(".new-link").addEventListener("click", function(){
     post_form.querySelector("input").focus();
 
     post_open_button.classList.add("open");
+
+    if(window.ga){
+      window.ga('send', 'event', "Links", "Button click", "New link form expanded", window.location.pathname.replace("/user/", ""));
+    }
   }
 });
 
@@ -177,5 +196,8 @@ post_form.addEventListener("submit", function(e){
   xhr.send( "title=" + encodeURIComponent(title.value) + "&link=" + encodeURIComponent(url.value) + "&user=" + encodeURIComponent(window.location.pathname.replace("/user/", "")) );
 
 
+  if(window.ga){
+    ga('send', 'event', "Links", "Button click", "New link form submitted", window.location.pathname.replace("/user/", ""));
+  }
 
 });
